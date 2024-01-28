a8a4462b91862808f0063c00bfe59a398c701379
unknown <ahmed.benchakhter@metave.us>
2024-01-28 18:25:15 +0000

    Finish the home page.

---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+interface LinkInfoInt {
+  linkPath: string
+  linkText: string
+  linkClasses?: string
+}
+
+export type { LinkInfoInt};
---
+++ src/utils/staticTextData.ts
@@ -0,0 +1,20 @@
+const helpSelfTipsList: string[] = [
+  "Try to be active and exercise.",
+  "Break large tasks into small ones.",
+  "Spend time with friends and family.",
+  "Set priorities."
+];
+
+const helpOthersTipsList: string[] = [
+  "Offer support and patience.",
+  "Invite him/her to outdoor activities e.g. walks.",
+  "Remind him about his medication frequently.",
+  "Encourage them that with time and treatment, depression will lift."
+];
+
+export { helpSelfTipsList, helpOthersTipsList };
+
+
+
+
+
