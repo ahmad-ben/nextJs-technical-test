c29774f305e04131d8d3e16b119f56c5af7f36a7
ahmad-ben <ahmadbenchakhti@gmail.com>
2024-01-28 22:13:42 +0000

    Finish the part two of the AboutUs Page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -32,7 +32,7 @@ export default function AboutPage() {
   return (
     <div className="aboutPageCom">
 
-      <header className="mx-52 mb-32">
+      <header className="mx-52 mb-24">
         <nav className="flex justify-end p-3 text-lg">
           <ul className="linksList flex items-center gap-8">
             {renderNavLinksList()}
@@ -42,7 +42,7 @@ export default function AboutPage() {
 
       <main>
         <div>
-          <section className="relative largeGreenBackground ps-3 me-48 flex gap-10 items-center">
+          <section className="relative largeGreenBackground ps-6 me-48 flex gap-10 items-center">
             <Image 
               src={upGreenWave} alt="Green Wave Icon" 
               className="wave absolute bottom-full"
@@ -66,14 +66,14 @@ export default function AboutPage() {
               className="wave absolute top-full"
             />
           </section>
-          <div className="my-32 text-xl">
+          <div className="px-4 mt-36 mb-28 text-xl">
             <p className="mb-10 me-48">
               Our mission is to empower individuals to prioritize their mental 
               well-being by providing accessible and high-quality mental health
               services. We envision a world where everyone has the opportunity 
               to achieve mental wellness and lead fulfilling lives.
             </p>
-            <p>
+            <p className="px-2 mb-3">
               Test offers a comprehensive suite of mental health services 
               tailored to meet your individual needs. Our team of experienced 
               and compassionate mental health professionals provides therapy, 
@@ -83,10 +83,14 @@ export default function AboutPage() {
               ensure personalized care that promotes growth, healing, and 
               resilience.
             </p>
-            <div className="flex items-center">
-              {/* <Image /> */}
+            <div className="flex items-center px-2">
+              <Image 
+                src="/icons/character-3.svg" alt="Sitting person"
+                width={175} height={400}
+                className=""
+              />
               <div>
-                <p className="mb-16">
+                <p className="mb-12 leading-10">
                   Our team consists of licensed therapists and counselors who 
                   are passionate about mental health and have extensive 
                   experience in their respective fields. They possess diverse 
@@ -95,7 +99,7 @@ export default function AboutPage() {
                   safe and supportive environment where you can explore your 
                   thoughts and emotions, and work towards positive change.
                 </p>
-                <p>
+                <p className="leading-10">
                   We understand the importance of privacy and confidentiality 
                   in the therapeutic process. At Bimo Health, we adhere to 
                   strict privacy standards and follow legal and ethical 
@@ -108,14 +112,31 @@ export default function AboutPage() {
           </div>
         </div>
         <div>
-          <section>
-            {/* <Image /> */}
-            <div>
-              <h3></h3>
-              <p></p>
-            </div>
+          <section className="relative largeGreenBackground ps-12 me-72">
+            <Image 
+              src={upGreenWave} alt="Green Wave Icon" 
+              className="wave absolute bottom-full"
+            />
+            <h3 className="text-6xl mb-3">Our Purpose</h3>
+            <p className="text-xl leading-10">
+              At Bimo Health, our purpose is deeply rooted in our belief that
+              solutions created to serve others, especially those who are 
+              underserved, are always the best solutions. We are driven by 
+              the commitment to make a positive impact on individuals' lives,
+              ensuring that everyone has access to the mental health care 
+              they deserve.
+            </p>
+            <Image 
+              src="/icons/character-1.svg" alt="Sitting person"
+              width={200} height={100}
+              className="absolute top-0 left-full z-10"
+            />
+            <Image 
+              src={downGreenWave} alt="Green Wave Icon" 
+              className="wave absolute top-full"
+            />
           </section>
-          <div>
+          <div className="mt-36">
             <p></p>
             <p></p>
             <p></p>
@@ -128,3 +149,6 @@ export default function AboutPage() {
     </div>
   );
 }
+
+
+// ?STOP In Girl chara
